9d592a0e8944ce2e0f06ec1039421cdd36f6fe47
AkintolaDavid <akintoladvaid66@gmail.com>
2024-08-07 11:10:03 +0100

    first

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const port = 5000;
+app.use(cors());
+app.use(bodyParser.json());
+app.post("/submit_aboutus_form", (req, res) => {
+  console.log(req.body); // Print the received data
+  res.send("Data received");
+});
+app.post("/submit_ourservices_form", (req, res) => {
+  console.log(req.body); // Print the received data
+  res.send("Data received");
+});
+app.post("/submit_contactus_form", (req, res) => {
+  console.log(req.body); // Print the received data
+  res.send("Data received");
+});
+app.post("/submit_letustalk_form", (req, res) => {
+  console.log(req.body); // Print the received data
+  res.send("Data received");
+});
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
